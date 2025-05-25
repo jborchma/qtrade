ad46e293435a00f46b1d56d2fd2450ecb2e20fa2
Jan Borchmann <j.borchmann@gmx.de>
2025-05-25 16:55:09 -0400

    adjusted readme

--- qtrade/__init__.py
+++ qtrade/__init__.py
@@ -1,4 +1,4 @@
-"""module init file
-"""
-from qtrade._version import __version__
-from qtrade.questrade import Questrade
+"""module init file"""
+
+from qtrade._version import __version__  # noqa: F401
+from qtrade.questrade import Questrade  # noqa: F401
